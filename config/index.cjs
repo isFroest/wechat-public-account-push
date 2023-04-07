74b560f978962d95cf1213977363e0a66540c0e3
isFroest <130153175+isFroest@users.noreply.github.com>
2023-04-07 20:50:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oN3j657XupCgAWHq0O1CknWyxlF0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9qjJif8sCmnavsedcKFzbExm5fZhfrGqEJULX-FiRXE',
+      useTemplateId: 'tna15ti-cJvqrKeG5UApgWtDatYGAlgGTlT1wpcTWSg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '8-18',
       festivals: [
@@ -47,13 +47,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '9qjJif8sCmnavsedcKFzbExm5fZhfrGqEJULX-FiRXE',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oN3j657XupCgAWHq0O1CknWyxlF0',
+      id: '',
     }
   ],
 
